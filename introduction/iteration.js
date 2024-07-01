7210f9456563a576421b31e4c5f70d7b8b5c85a7
tahircanyildiz <tahircanyildiz@hotmail.com>
2024-07-01 17:02:27 +0300

    Monday Last Commit

--- introduction/iteration.js
+++ introduction/iteration.js
@@ -94,3 +94,91 @@ const q4 = ["Oct", "Nov", "May"];
 
 const year = [...q1, ...q2, ...q3, ...q4];
 document.getElementById("demo11").innerHTML = year; 
+
+
+
+// Create an Array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
+
+// Display the Array:
+document.getElementById("demo").innerHTML = cars; 
+
+//MAP deneme
+const map = new Map();
+// Eleman ekleme
+map.set('name', 'John');
+map.set('age', 25);
+map.set('country', 'USA');
+// Elemanları okuma
+console.log(map.get('name')); // Output: John
+console.log(map.get('age')); // Output: 25
+// Eleman kontrolü
+console.log(map.has('country')); // Output: true
+console.log(map.has('city')); // Output: false
+// Eleman sayısı
+console.log(map.size); // Output: 3
+// Eleman silme
+map.delete('age');
+console.log(map.size); // Output: 2
+// Tüm elemanları silme
+map.clear();
+console.log(map.size); // Output: 0
+
+
+// Anahtarlar üzerinde gezinme
+for (const key of map.keys()) {
+  console.log(key);
+}
+// Output: name, age, country
+
+// Değerler üzerinde gezinme
+for (const value of map.values()) {
+  console.log(value);
+}
+// Output: Jane, 30, Canada
+
+// Anahtar-değer çiftleri üzerinde gezinme
+for (const [key, value] of map.entries()) {
+  console.log(`${key}: ${value}`);
+}
+// Output: name: Jane, age: 30, country: Canada
+
+//Javascript iterators
+const array = [1, 2, 3];
+const iterator = array[Symbol.iterator]();
+
+console.log(iterator.next()); // { value: 1, done: false }
+console.log(iterator.next()); // { value: 2, done: false }
+console.log(iterator.next()); // { value: 3, done: false }
+console.log(iterator.next()); // { value: undefined, done: true }
+
+
+//Iterator oluşturma
+const myIterable = {
+  [Symbol.iterator]() {
+      let step = 0;
+      const iterator = {
+          next() {
+              step++;
+              if (step === 1) {
+                  return { value: 'First', done: false };
+              } else if (step === 2) {
+                  return { value: 'Second', done: false };
+              } else {
+                  return { value: undefined, done: true };
+              }
+          }
+      };
+      return iterator;
+  }
+};
+for (const value of myIterable) {console.log(value);}
+// Output:
+// First
+// Second
